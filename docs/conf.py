dc2a913a20ee6a5f255efea79eba9584e7db87b9
phip123 <poepsi39@gmail.com>
2021-03-23 17:43:44 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ author = 'sphinx-action Test Suite'
 
 
 # -- General configuration ---------------------------------------------------
-
+import sphinx_rtd_theme
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
